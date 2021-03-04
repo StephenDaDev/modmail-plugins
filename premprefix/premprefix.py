24968d812c419018cf064e31057cba67749c1e9e
Stephen <48072084+StephenDaDev@users.noreply.github.com>
2021-03-04 08:35:30 -0500

    Create plugin

---
+++ premprefix/premprefix.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from core import checks
+
+
+class PremPrefix(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    @checks.thread_only()
+    async def raw(self, ctx, message_id: int=None):
+        await ctx.send('<<prem>>')
+        
+
+def setup(bot):
+    bot.add_cog(PremPrefix(bot))
